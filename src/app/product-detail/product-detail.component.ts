6be55ef171b43319616cf8ac8b9671d528abc4a6
eriollee <eriollee@163.com>
2017-08-05 00:13:09 +0800

    增加星级评论
    利用 input output 双向绑定 onChanges 及Array.reduce方法递归调用加和

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,12 +12,33 @@ export class ProductDetailComponent implements OnInit {
 
   comments:Comment[];
 
+  newRating:number=5;
+
+  newComment:string = "";
+
+  isCommentHidden = true; 
+
+
   constructor(private routeInfo:ActivatedRoute,private productService:ProductService) { }
 
   ngOnInit() {
   	let productId:number = this.routeInfo.snapshot.params["prodId"];
   	this.product = this.productService.getProductId(productId);
   	this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  
+  addComment(){
+    let comment = new Comment(0,this.product.id,new Date().toISOString(),"Eriollee",this.newRating,this.newComment);
+    this.comments.unshift(comment);  
+    /*恢复默认值*/
+    this.newComment =null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+
+    /*计算平均值*/
+    let sum = this.comments.reduce((sum,comment)=>sum+comment.rating,0);
+    this.product.rating = sum / this.comments.length;
   }
 
 
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input,Output,EventEmitter,SimpleChanges,OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit,OnChanges {
   
 
   @Input()
   private rating: number = 0;
 
+  @Output()
+  private ratingChange:EventEmitter<number> = new EventEmitter();
+
+
   private stars:boolean[];
+
+  @Input()
+  private readonly:boolean = true;
+
   constructor() { }
 
   ngOnInit() {
 
-  	this.stars = [];
-  	for(let i=1 ;i<=5;i++){
-  		this.stars.push(i > this.rating	);
-  	}
+  	
   	/*this.stars =[false,false,false,false,true];*/
+
   }
+  
+  ngOnChanges(changes:SimpleChanges):void {
+    this.stars = [];
+    for(let i=1 ;i<=5;i++){
+      this.stars.push(i > this.rating  );
+    }
+ 
+  }
+
+
+    clickStar(index:number){
+      if(!this.readonly){
+        this.rating = index+1;
+        this.ratingChange.emit(this.rating);
+      }
+    }
 
 }
